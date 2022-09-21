54dad343a1bee88939a321c06c7075324ad8b6d8
ydsahu1980 <sahu.yagnadutta@gmail.com>
2022-09-21 13:21:00 -0700

    First Commit

---
+++ firstPro/src/main/java/com/example/demo/FirstProApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProApplication.class, args);
+		System.out.println("This is first pro");
+	}
+
+}
